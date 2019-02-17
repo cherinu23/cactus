bcd59ea243fa50d5a0fa7e1da3f3aa5fe3ffddf9
Alexandru Rotaru <rotaru.alexandru23@yahoo.com>
2019-02-17 16:29:41 +0200

    modified:   resources/templates/header.php 	new file:   resources/templates/reset.html 	new file:   resources/templates/taskpage.html

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Cactus</title>
 	<link href="<?php echo CSS_DIR ?>css/bootstrap-4.2.1.min.css" rel="stylesheet" type="text/css"/>
-	<link href="<?php echo CSS_DIR ?>css/all.css" rel="stylesheet" type="text/css"/>
+	<link href="<?php echo CSS_DIR ?>css/fontawesome-5.7.1.all.css" rel="stylesheet" type="text/css"/>
 	<link href="<?php echo CSS_DIR ?>css/custom.css" rel="stylesheet" type="text/css"/>
 </head>
 

e2047636d1d1946b9e34f2899ac5730920015f04
Alexandru Rotaru <rotaru.alexandru23@yahoo.com>
2019-04-04 22:27:51 +0300

    modified:   resources/library/contry.php

--- resources/library/contry.php
+++ resources/library/contry.php
@@ -12,43 +12,61 @@
  $errors = array();
 
 // connect to db
-// object oriented style
 
- $db1 = new mysqli($config['db']['db1']['host'], $config['db']['db1']['username'], $config['db']['db1']['password'], $config['db']['db1']['dbname']);
+ $mysqli = new mysqli($config['db']['db1']['host'], $config['db']['db1']['username'], $config['db']['db1']['password'], $config['db']['db1']['dbname']);
  
 // check connection
 
- if (mysqli_connect_error()) {
- 	printf("Connect failed: %s\n", mysqli_connect_error());
- 	exit();
+ if ($mysqli->connect_errno) {
+ 	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
  }
 
 // register users
 
  if(isset($_POST['username'])){
- 	$username = mysqli_real_escape_string($db1, $_POST['username']);
- }
+ 	$username = mysqli_real_escape_string($mysqli, $_POST['username']);
+ } 
+
  if(isset($_POST['email'])){
- 	$email = mysqli_real_escape_string($db1, $_POST['email']);
- }
+ 	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+ } 
  if(isset($_POST['password'])){
- 	$password = mysqli_real_escape_string($db1, $_POST['password']);
- }
+ 	$password = mysqli_real_escape_string($mysqli, $_POST['password']);
+ } 
  if(isset($_POST['confirm_password'])){
- 	$confirm_password = mysqli_real_escape_string($db1, $_POST['confirm_password']);
+ 	$confirm_password = mysqli_real_escape_string($mysqli, $_POST['confirm_password']);
  }
-
-
 
 //form validation
 
+ if(empty($username)) {array_push($errors, "Username is required");};
+ if(empty($email)) {array_push($errors, "email is required");};
+ if(empty($password)) {array_push($errors, "Username is required");};
+ if($password != $confirm_password){array_push($errors, "Password do not match");};
 
+ //check for duplicates using prepared statement
 
-// check DB for user duplicate
+ $chkd = $mysqli->prepare("SELECT * FROM user WHERE username = ? or email = ?");
+ $chkd->bind_param("ss", $username, $email);
+ $chkd->execute();
+ $chkd->store_result();
+ 
+ $numRows = $chkd->num_rows();
+ if( $numRows )
+ {
+ 	echo "<p style='text-align: center; color:red'>Username/Email is already registered</p>";
 
+ } else {
 
+ 	$secon = $mysqli->prepare("INSERT INTO user (username, email, password) VALUES (?, ?, ?)");
+ 	$secon->bind_param("sss", $username, $email, $password);
+ 	$secon->execute();
+ 	$secon->close();
+ 	$chkd->close();
 
-// Register user if there is no error
+ };
+
+ // header('Location:resources/templates/home.php');
 
 
 
@@ -70,9 +88,10 @@
 
 // [TEST CASE 2]
 // dumps information about a variable and prints human-readable information about a variable
- var_dump($email);
-// print_r($password)
+   // var_dump($check_duplicates);
+ // print_r($query)
 // [END TEST CASE 2]
 
-
+// header('Location:resources/templates/home.php');
+ $mysqli->close();
  ?>
